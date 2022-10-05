5d614896722e2f676dbd990573447ad9e65d92ef
itoemon <daichi.eightgood@gmail.com>
2022-10-05 15:33:56 +0900

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,13 @@ import { useRecoilState, useRecoilValue } from "recoil";
 const Home = () => {
 
   return (
-    <div>a</div>
+  <div>
+    <a href="https://lin.ee/1Xj2SQF">
+      <img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"/>
+    </a>
+    {/* <div class="line-it-button" data-lang="ja" data-type="friend" data-env="REAL" data-count="true" data-home="true" data-lineId="@443jtuss" style="display: none;"></div>
+      <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script> */}
+  </div>
   );
 };
 
