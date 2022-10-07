f59c9458d55030c5bb866bd77bbf93c3afac620f
itoemon <daichi.eightgood@gmail.com>
2022-10-07 20:17:38 +0900

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
     body: new URLSearchParams({
         'grant_type': 'authorization_code',
         'code': `${query2.get('code')}`,
-        'redirect_uri' : `${encodeURI("https://gojigoji.web.app/callback")}`,
+        'redirect_uri' : `${encodeURI("https://gojigoji.web.app")}`,
         'client_id': '1657533055',
         'client_secret': 'a05e08108d51be250977f137a61ef88d'
     })
@@ -91,7 +91,7 @@ const Home = () => {
     <a href="https://lin.ee/1Xj2SQF">
       <img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"/>
     </a>
-    <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657533055&redirect_uri=https://gojigoji.web.app/callback&state=1234asdg&bot_prompt=aggressive&scope=profile%20openid%20email&"
+    <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657533055&redirect_uri=https://gojigoji.web.app&state=1234asdg&bot_prompt=aggressive&scope=profile%20openid%20email&"
       alt="">
         ログイン
     </a>
